b04634c89f60649be3e3d76a41dc93192c0157b2
felit <felit123456>
2016-05-22 01:23:44 +0800

    添加　java并发编程战　示例

---
+++ src/main/java/com/livedrof/j2se/concurrent/in_practice/CoutingFactorizer.java
@@ -0,0 +1,60 @@
+package com.livedrof.j2se.concurrent.in_practice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ */
+public class CoutingFactorizer {
+    private final AtomicLong count = new AtomicLong();
+
+    public long getCount() {
+        return count.get();
+    }
+
+    public void plus() {
+        count.incrementAndGet();
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        final CoutingFactorizer cf = new CoutingFactorizer();
+        final UnsafeCounting uc = new UnsafeCounting();
+        final int num = 100000;
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < num; i++) {
+                    cf.plus();
+                    uc.plus();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < num; i++) {
+                    cf.plus();
+                    uc.plus();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        Thread.currentThread().sleep(1000);
+        System.out.println(cf.getCount());
+        System.out.println(uc.getCount());
+    }
+
+    static class UnsafeCounting {
+        private int num;
+
+        public long getCount() {
+            return num;
+        }
+
+        public void plus() {
+            num++;
+        }
+    }
+}
