95c2592cf4bbc6765bb3d8b7e52fa240ad46b3f0
congshuanglong <congshuanglong@yazuo.com>
2018-03-01 10:23:23 +0800

    GC示例1

---
+++ src/main/java/com/livedrof/jvm/gc/AllocEden.java
@@ -0,0 +1,11 @@
+package com.livedrof.jvm.gc;
+
+public class AllocEden {
+    public static final int _1K = 1024;
+
+    public static void main(String args[]) {
+        for(int i=0;i< 5* _1K; i++) {
+            byte[] b = new byte[_1K];
+        }
+    }
+}
