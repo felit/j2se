5f5bde5ef84d69a3543dd6417472432b44468a22
congshuanglong <congshuanglong@126.com>
2022-10-30 13:09:50 +0800

    update

--- src/test/java/com/livedrof/nio/MemoryChannelTest.java
+++ src/test/java/com/livedrof/nio/MemoryChannelTest.java
@@ -8,18 +8,35 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MemoryChannelTest {
     @Test
-    public void testChannel() throws IOException {
+    public void testChannelWrite() throws IOException {
 
         String filename = "sss.mmap.txt";
         FileChannel fileChannel = this.getWritableChannel(filename);
         MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 6);
         // 会把ByteBuffer全部写入文件;即使是空位
         mappedByteBuffer.put("中言".getBytes());
+        fileChannel.close();
+    }
+
+    @Test
+    public void testChannelRead() throws IOException {
+        String filename = "sss.mmap.txt";
+        FileChannel fileChannel = this.getReadChannel(filename);
+        MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, 6);
+        // java.nio.BufferUnderflowException
+        byte[] bytes = new byte[10];
+        // 会把ByteBuffer全部写入文件;即使是空位
+        mappedByteBuffer.get(bytes,0,mappedByteBuffer.limit());
+        for (int i = 0; i < 6; i++) {
+            System.out.println(bytes[i]);
+        }
+        System.out.printf(new String(bytes, Charset.defaultCharset()));
         fileChannel.close();
     }
 
